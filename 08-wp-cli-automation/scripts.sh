4dffe8dd24a369c69732e154b24aef8fc4ed3317
Sindie <sindie2465@gmail.com>
2026-05-07 01:57:32 +0200

    Essentials CLI script for WP-CLI automation
    Added essentials CLI script for automating WP-CLI tasks, including installation of essential plugins and deletion of inactive themes.

--- 08-wp-cli-automation/scripts.sh
+++ 08-wp-cli-automation/scripts.sh
@@ -1,2 +1,24 @@
+#Essentials CLI Script
+----------------------------------
 #!/bin/bash
-# Example automation script for WP-CLI tasks
+
+# Install essential plugins
+PLUGINS=(
+  "advanced-custom-fields"
+  "elementor"
+  "wpforms-lite"
+  "rank-math"
+  "wp-super-cache"
+  "wordfence"
+)
+
+for PLUGIN in "${PLUGINS[@]}"; do
+  wp plugin install "$PLUGIN" --activate --allow-root
+done
+
+echo "All essential plugins installed and activated!"
+
+###########################################################################################################################################
+
+#Delete all inactive themes:
+wp theme delete $(wp theme list --status=inactive --field=name)
